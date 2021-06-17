d510f1e1a9968235483fbd439acae3de5d6fb199
Alex <alexroth96@gmail.com>
2021-06-17 10:57:21 -0400

    create the othe end points

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,10 @@ const { default: axios } = require("axios");
 const express = require("express");
 const router = express.Router();
 
-//@route GET
-//@desc Grab data from click funnels webhook
+//@route POST
+//@desc POST data from click funnels webhook to eveflow
 //@access private
+//@STORE: BOOM BUDS ENDPOINT
 router.post("/", async (req, res) => {
     // console.log("data", req.headers);
     const currentDate = new Date();
@@ -49,6 +50,211 @@ router.post("/", async (req, res) => {
         console.log("affiliate sub id", aff_sub)
 
         const url = !contact.upsell ? `https://www.poptrkr.com/?nid=577&transaction_id=${aff_sub}&amount=${formattedAmount}` : `https://www.poptrkr.com/?nid=577&aid=6&adv_event_id=7&transaction_id=${aff_sub}&amount=${formattedAmount}`
+
+        const response = await axios({
+            url,
+            method: "POST",
+            headers: {
+                "content-type": 'application/json',
+                "X-Eflow-API-Key": process.env.EF_API
+            },
+        })
+
+        console.log('response to webhook', response.data)
+
+        return res.status(200).json(response.data);
+    } catch (error) {
+        console.error("ERROR:", error);
+        return res.status(500).json({ msg: "Uh Oh" });
+    }
+});
+
+//@route POST
+//@desc POST data from click funnels webhook to eveflow
+//@access private
+//@STORE: LUCIANA ROSE
+//EACH AID AND ADV_EVENT IS DIFFERENT
+router.post("/lulu", async (req, res) => {
+    // console.log("data", req.headers);
+    const currentDate = new Date();
+    const utcDate = currentDate.toISOString() + " UTC"
+    req.headers["content-type"] = "application/json";
+    req.headers["X-Clickfunnels-Webhook-Delivery-Id"] =
+        process.env.CF_DELIVERY_ID;
+    req.headers["payload"] = { "time": utcDate }
+
+
+    try {
+
+        const { purchase, event } = req.body
+
+        if (!purchase) return res.status(200).send({ msg: 'Not a purchase event' });
+
+        console.log('type:', event, "purchase: ", purchase);
+
+        const { products, status, error_message, contact } = purchase
+
+        const productAmount = products.reduce((total, product) => {
+            return total + parseFloat(product?.amount?.cents)
+        }, 0)
+
+        console.log('contact info', contact, "CONTACT UPSELL:", contact.upsell)
+
+        const formattedAmount = (productAmount / 100).toFixed(2);
+
+        console.log('product amount', formattedAmount, status, error_message);
+        //handle cards being declined
+        if (status === 'failed') {
+            console.log(error_message)
+            return res.status(200).json(error_message)
+        }
+
+        const { aff_sub } = purchase.contact
+
+        if (!aff_sub) {
+            console.log('No affiliate id associated with purchase')
+            return res.status(200).json({ msg: 'No affiliate id associated with purchase' })
+        }
+        console.log("affiliate sub id", aff_sub)
+
+        const url = !contact.upsell ? `https://www.poptrkr.com/?nid=577&transaction_id=${aff_sub}&amount=${formattedAmount}` : `https://www.poptrkr.com/?nid=577&aid=5&adv_event_id=9&transaction_id=${aff_sub}&amount=${formattedAmount}`
+
+        const response = await axios({
+            url,
+            method: "POST",
+            headers: {
+                "content-type": 'application/json',
+                "X-Eflow-API-Key": process.env.EF_API
+            },
+        })
+
+        console.log('response to webhook', response.data)
+
+        return res.status(200).json(response.data);
+    } catch (error) {
+        console.error("ERROR:", error);
+        return res.status(500).json({ msg: "Uh Oh" });
+    }
+});
+
+
+//@route POST
+//@desc POST data from click funnels webhook to eveflow
+//@access private
+//@STORE: PG
+//EACH AID AND ADV_EVENT IS DIFFERENT
+router.post("/pg", async (req, res) => {
+    // console.log("data", req.headers);
+    const currentDate = new Date();
+    const utcDate = currentDate.toISOString() + " UTC"
+    req.headers["content-type"] = "application/json";
+    req.headers["X-Clickfunnels-Webhook-Delivery-Id"] =
+        process.env.CF_DELIVERY_ID;
+    req.headers["payload"] = { "time": utcDate }
+
+
+    try {
+
+        const { purchase, event } = req.body
+
+        if (!purchase) return res.status(200).send({ msg: 'Not a purchase event' });
+
+        console.log('type:', event, "purchase: ", purchase);
+
+        const { products, status, error_message, contact } = purchase
+
+        const productAmount = products.reduce((total, product) => {
+            return total + parseFloat(product?.amount?.cents)
+        }, 0)
+
+        console.log('contact info', contact, "CONTACT UPSELL:", contact.upsell)
+
+        const formattedAmount = (productAmount / 100).toFixed(2);
+
+        console.log('product amount', formattedAmount, status, error_message);
+        //handle cards being declined
+        if (status === 'failed') {
+            console.log(error_message)
+            return res.status(200).json(error_message)
+        }
+
+        const { aff_sub } = purchase.contact
+
+        if (!aff_sub) {
+            console.log('No affiliate id associated with purchase')
+            return res.status(200).json({ msg: 'No affiliate id associated with purchase' })
+        }
+        console.log("affiliate sub id", aff_sub)
+
+        const url = !contact.upsell ? `https://www.poptrkr.com/?nid=577&transaction_id=${aff_sub}&amount=${formattedAmount}` : `https://www.poptrkr.com/?nid=577&aid=1&adv_event_id=5&transaction_id=${aff_sub}&amount=${formattedAmount}`
+
+        const response = await axios({
+            url,
+            method: "POST",
+            headers: {
+                "content-type": 'application/json',
+                "X-Eflow-API-Key": process.env.EF_API
+            },
+        })
+
+        console.log('response to webhook', response.data)
+
+        return res.status(200).json(response.data);
+    } catch (error) {
+        console.error("ERROR:", error);
+        return res.status(500).json({ msg: "Uh Oh" });
+    }
+});
+
+//@route POST
+//@desc POST data from click funnels webhook to eveflow
+//@access private
+//@STORE: SLICK
+//EACH AID AND ADV_EVENT IS DIFFERENT
+router.post("/slick", async (req, res) => {
+    // console.log("data", req.headers);
+    const currentDate = new Date();
+    const utcDate = currentDate.toISOString() + " UTC"
+    req.headers["content-type"] = "application/json";
+    req.headers["X-Clickfunnels-Webhook-Delivery-Id"] =
+        process.env.CF_DELIVERY_ID;
+    req.headers["payload"] = { "time": utcDate }
+
+
+    try {
+
+        const { purchase, event } = req.body
+
+        if (!purchase) return res.status(200).send({ msg: 'Not a purchase event' });
+
+        console.log('type:', event, "purchase: ", purchase);
+
+        const { products, status, error_message, contact } = purchase
+
+        const productAmount = products.reduce((total, product) => {
+            return total + parseFloat(product?.amount?.cents)
+        }, 0)
+
+        console.log('contact info', contact, "CONTACT UPSELL:", contact.upsell)
+
+        const formattedAmount = (productAmount / 100).toFixed(2);
+
+        console.log('product amount', formattedAmount, status, error_message);
+        //handle cards being declined
+        if (status === 'failed') {
+            console.log(error_message)
+            return res.status(200).json(error_message)
+        }
+
+        const { aff_sub } = purchase.contact
+
+        if (!aff_sub) {
+            console.log('No affiliate id associated with purchase')
+            return res.status(200).json({ msg: 'No affiliate id associated with purchase' })
+        }
+        console.log("affiliate sub id", aff_sub)
+
+        const url = !contact.upsell ? `https://www.poptrkr.com/?nid=577&transaction_id=${aff_sub}&amount=${formattedAmount}` : `https://www.poptrkr.com/?nid=577&aid=7&adv_event_id=10&transaction_id=${aff_sub}&amount=${formattedAmount}`
 
         const response = await axios({
             url,
